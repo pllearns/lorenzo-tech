99767513f05c901f9b55fc5af8a9116c53d76ef6
pllearns <phill.lorenzo@outlook.com>
2021-11-21 14:45:38 -0800

    setup: additional setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import { Footer, Blog, Features, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 const App = () => {
   return (
-    <div>
-      <h1>Lorenzo Tech</h1>
+    <div className="App">
+      <div clasName="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Features />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
